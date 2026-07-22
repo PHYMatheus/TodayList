e767fb5bf75e4826041e81c939161fdc6dba6cda
PHYMatheus <matheusguilherme00221@gmail.com>
2026-07-22 12:03:10 -0300

    Configuração do banco postgresql

--- todaylist/src/main/java/br/com/todaylist/user/UserModel.java
+++ todaylist/src/main/java/br/com/todaylist/user/UserModel.java
@@ -1,9 +1,27 @@
 package br.com.todaylist.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "tb_Users") // <- informs the database it is a table
+@Data // <- import Getters e Setters
 public class UserModel {
+
+    @Id // <- informs the database that it is a primary key
+    @GeneratedValue(generator = "UUID") //<- Generates an ID in the format
+    private UUID id;
 
     private String username;
     private String password;
     private String name;
-
+    @Column(name = "date create")
+    @CreationTimestamp // <- the database saves data when it is created
+    private LocalDateTime createdAt;
 }
