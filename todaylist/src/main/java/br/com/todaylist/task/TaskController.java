6217d94c61a6507ac808b4993772cc47936893fc
PHYMatheus <matheusguilherme00221@gmail.com>
2026-07-23 11:40:08 -0300

    Feat: implementação de Validações em Data e User

--- todaylist/src/main/java/br/com/todaylist/task/TaskController.java
+++ todaylist/src/main/java/br/com/todaylist/task/TaskController.java
@@ -2,10 +2,14 @@ package br.com.todaylist.task;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 import java.util.UUID;
 
 @RestController
@@ -16,11 +20,21 @@ public class TaskController {
     private TaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel createTask(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity createTask(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
 
+        var currentDate = LocalDate.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Data de Inicio ou Término precisa ser maior que á atual");
+        }
+
+        if(taskModel.getStartAt().isBefore(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data de Inicio precisa ser menor que á de Término!");
+        }
+
         var task = this.taskRepository.save(taskModel);
-           return task;
+           return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
